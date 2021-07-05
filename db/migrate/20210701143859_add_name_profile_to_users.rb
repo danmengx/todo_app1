2afcd4e4f25f92c5ff59601d1035e431e65c806d
EC2 Default User <ec2-user@ip-172-31-46-183.us-west-2.compute.internal>
2021-07-05 10:55:57 +0000

    7/5

---
+++ db/migrate/20210701143859_add_name_profile_to_users.rb
@@ -0,0 +1,5 @@
+class AddNameProfileToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :company, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_20_030200) do
+ActiveRecord::Schema.define(version: 2021_07_01_143859) do
 
   create_table "goals", force: :cascade do |t|
     t.string "title"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2021_06_20_030200) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "company"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
